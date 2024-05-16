b3d665b489f25ab298ad762eaf8a13f66e39301d
LarsAssen <juniorassen@hotmail.com>
2024-05-16 16:12:42 +0200

    shit is fucked

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,18 @@
-module.exports = () => ({});
+module.exports = ({ env }) => {
+    return {
+      upload: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+          secure: true,
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    };
+  };
+  
